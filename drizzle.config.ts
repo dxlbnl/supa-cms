5c2e4b599f544d4b4a390ebbea08b75fd4487c80
Dexter <dexter@dxlb.nl>
2023-10-18 23:18:13 +0200

    Setup edit image resizing

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,12 @@
-import type { Config } from "drizzle-kit";
-import * as dotenv from "dotenv";
+import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
 dotenv.config();
-console.log(process.env.DATABASE_URL);
 
 export default {
-  schema: "./src/schema/*",
-  out: "./drizzle",
-  driver: 'pg',
-  dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
-  }
+	schema: './src/schema/*',
+	out: './drizzle',
+	driver: 'pg',
+	dbCredentials: {
+		connectionString: process.env.DATABASE_URL
+	}
 } satisfies Config;
