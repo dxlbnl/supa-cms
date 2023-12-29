20392279135f9beb0dcf86a56518e6a9ebfe3df2
Dexter <dexter@dxlb.nl>
2023-12-29 21:16:02 +0100

    restored sample content

--- src/routes/ohlijf/+page.svelte
+++ src/routes/ohlijf/+page.svelte
@@ -1,56 +1,30 @@
-<script>
-	import Content from '$lib/components/Content.svelte';
-	import Header from '$lib/components/Header.svelte';
-	import Navigation from '$lib/components/Navigation.svelte';
-	import WieZijnWij from '$lib/components/WijZijnWij.svelte';
-</script>
-
-<Navigation />
-
-<Header>
-	<h1 class="feature">Een lichter leven zonder pijn</h1>
-	<p class="feature">Wie we zijn, wat we doen en hoe we iemands leven beter maken.</p>
-	<button class="cta">Ontdek onze methode</button>
-</Header>
-
-<Content>
-	<section class="columns">
-		<div>
-			<h2>De verlamming van pijn..</h2>
-			<p>
-				Wij kennen het maar al te goed!<br />Je hebt alles geprobeerd, maar de behandeling werkt
-				niet of maar tijdelijk. Je pijn of andere symptomen zorgen ervoor dat je niet alles kan doen
-				wat je zou willen. En dat frustreert je!<br />
-			</p>
-			<a href="#"> Lees meer over de symptomen.. </a>
-		</div>
-		<div class="frame">
-			<img class="Image11" src="/pain.jpeg" />
-		</div>
-	</section>
-
-	<section>
-		<h1>De verlamming van Chronische pijn..</h1>
-		<p>Wij kennen het maar al te goed!</p>
-		<p>Je hebt alles geprobeerd, maar de behandeling werkt niet of maar tijdelijk.</p>
+<section class="columns">
+	<div>
+		<h2>De verlamming van pijn..</h2>
 		<p>
-			Je pijn of andere symptomen zorgen ervoor dat je niet alles kan doen wat je zou willen. En dat
-			frustreert je!
+			Wij kennen het maar al te goed!<br />Je hebt alles geprobeerd, maar de behandeling werkt niet
+			of maar tijdelijk. Je pijn of andere symptomen zorgen ervoor dat je niet alles kan doen wat je
+			zou willen. En dat frustreert je!<br />
 		</p>
-		<a href="/symptomen"> Lees meer over de symptomen.. </a>
-
-		<img src="art-chronische-pijn.svg" />
-	</section>
-</Content>
-
-<WieZijnWij />
-
-<section class="content">
-	<p>Download ons selfcare werkboek</p>
-	<button>Gratis download!</button>
+		<a href="#"> Lees meer over de symptomen.. </a>
+	</div>
+	<div class="frame">
+		<img class="Image11" src="/pain.jpeg" />
+	</div>
 </section>
 
-<footer></footer>
+<section>
+	<h1>De verlamming van Chronische pijn..</h1>
+	<p>Wij kennen het maar al te goed!</p>
+	<p>Je hebt alles geprobeerd, maar de behandeling werkt niet of maar tijdelijk.</p>
+	<p>
+		Je pijn of andere symptomen zorgen ervoor dat je niet alles kan doen wat je zou willen. En dat
+		frustreert je!
+	</p>
+	<a href="/symptomen"> Lees meer over de symptomen.. </a>
+
+	<img src="art-chronische-pijn.svg" />
+</section>
 
 <style>
 	.frame {
@@ -61,10 +35,5 @@
 		border: 3px var(--green) solid;
 
 		align-self: center;
-	}
-
-	footer {
-		height: 7rem;
-		background: #8bc385;
 	}
 </style>
